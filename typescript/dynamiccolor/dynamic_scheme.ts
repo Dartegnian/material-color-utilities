eff791ff850e1f19dca45813f7a0cfd6c076f6dc
Dartegnian <root@dartegnian.com>
2025-07-12 19:45:49 +0800

    Update package, use 2025 spec

--- typescript/dynamiccolor/dynamic_scheme.ts
+++ typescript/dynamiccolor/dynamic_scheme.ts
@@ -114,7 +114,7 @@ interface DynamicSchemePalettesDelegate {
  * with the theme style. Used by DynamicColor to resolve into a color.
  */
 export class DynamicScheme {
-  static readonly DEFAULT_SPEC_VERSION = '2021';
+  static readonly DEFAULT_SPEC_VERSION = '2025';
   static readonly DEFAULT_PLATFORM = 'phone';
 
   /**
